6bb8c207e2a44d26292174fbd803bc2792aff54e
uraganNN333 <farmak8029@gmail.com>
2025-07-11 18:52:41 +0300

    Add files via upload

---
+++ Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <unordered_set>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> charSet;
+        int left = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.size(); right++) {
+            while (charSet.find(s[right]) != charSet.end()) {
+                charSet.erase(s[left]);
+                left++;
+            }
+            charSet.insert(s[right]);
+            maxLength = max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+};
+
+int main() {
+    string input;
+    cout << "Введите строку: ";
+    getline(cin, input);  // Позволяет вводить строку с пробелами
+
+    Solution sol;
+    int result = sol.lengthOfLongestSubstring(input);
+
+    cout << "Длина самой длинной подстроки без повторяющихся символов: " << result << endl;
+
+    return 0;
+}
