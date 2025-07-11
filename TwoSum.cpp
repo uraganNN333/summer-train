0d91dd52bd30497376a82f29f304040c5023dca1
uraganNN333 <farmak8029@gmail.com>
2025-07-11 18:48:31 +0300

    Add files via upload

---
+++ TwoSum.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return { i, j };
+                }
+            }
+        }
+        return {};
+    }
+};
+
+int main() {
+    // Пример входных данных
+    vector<int> nums = { 2, 7, 11, 15 };
+    int target = 9;
+
+    Solution sol;
+    vector<int> result = sol.twoSum(nums, target);
+
+    if (!result.empty()) {
+        cout << "Индексы: " << result[0] << " и " << result[1] << endl;
+    }
+    else {
+        cout << "Пара не найдена." << endl;
+    }
+
+    return 0;
+}
